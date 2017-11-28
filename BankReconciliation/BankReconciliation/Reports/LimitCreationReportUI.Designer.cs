2a29d09a3a9141a98fe1feaca6c4b9d6ae94350b
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-11-28 14:07:34 +0600

    UI modified

---
+++ BankReconciliation/BankReconciliation/Reports/LimitCreationReportUI.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace BankReconciliation.Reports
+{
+    partial class LimitCreationReportUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LimitCreationReportUI));
+            this.button10 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.accountNoComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bankSelectionComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Snow;
+            this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.5F, System.Drawing.FontStyle.Bold);
+            this.button10.ForeColor = System.Drawing.Color.Green;
+            this.button10.Location = new System.Drawing.Point(384, 207);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(138, 51);
+            this.button10.TabIndex = 21;
+            this.button10.Text = "Get";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.5F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(140, 29);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(279, 26);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Select Bank and Account";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 154);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 24);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Account No";
+            // 
+            // accountNoComboBox
+            // 
+            this.accountNoComboBox.FormattingEnabled = true;
+            this.accountNoComboBox.Location = new System.Drawing.Point(221, 154);
+            this.accountNoComboBox.Margin = new System.Windows.Forms.Padding(6);
+            this.accountNoComboBox.Name = "accountNoComboBox";
+            this.accountNoComboBox.Size = new System.Drawing.Size(301, 21);
+            this.accountNoComboBox.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 98);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 24);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Bank Name";
+            // 
+            // bankSelectionComboBox
+            // 
+            this.bankSelectionComboBox.FormattingEnabled = true;
+            this.bankSelectionComboBox.Items.AddRange(new object[] {
+            "AB Bank Limited",
+            "Agrani Bank Limited",
+            "Al-Arafah Islami Bank Limited ",
+            "Bangladesh Commerce Bank Limited",
+            "Bangladesh Development Bank Limited",
+            "Bangladesh Krishi Bank Limited",
+            "Bank Al-Falah",
+            "Bank Asia Limited",
+            "BASIC Bank Limited",
+            "BRAC Bank Limited",
+            "Citibank NA",
+            "Commercial Bank of Ceylon",
+            "Dhaka Bank Limited",
+            "Dutch Bangla Bank Limited",
+            "Eastern Bank Limited",
+            "Export Import Bank of Bangladesh Limited",
+            "First Security Islami Bank Limited",
+            "Habib Bank Limited",
+            "HSBC (The Hong Kong and Shanghai Banking Corporation Ltd.)",
+            "ICB Islamic Bank Limited",
+            "IFIC Bank Limited",
+            "Islami Bank Bangladesh Limited",
+            "Jamuna Bank Limited",
+            "Janata Bank Limited",
+            "Meghna Bank Limited",
+            "Mercantile Bank Limited",
+            "Midland Bank Limited",
+            "Modhumoti Bank Limited",
+            "Mutual Trust Bank Limited",
+            "National Bank Limited",
+            "National Bank of Pakistan",
+            "NCC Bank Limited",
+            "NRB Bank Limited",
+            "NRB Commercial Bank Limited",
+            "NRB Global Bank Limited",
+            "One Bank Limited",
+            "Palli Sanchay Bank",
+            "Prime Bank Limited",
+            "Pubali Bank Limited",
+            "Rajshahi Krishi Unnoyon Bank (RKUB)",
+            "Rupali Bank Limited",
+            "Shahjalal islami Bank Limited",
+            "Shimanto Bank Limited",
+            "Social Islami Bank Limited ",
+            "Sonali Bank Limited ",
+            "South Bangla Agriculture and Commerce Bank Limited ",
+            "Southeast Bank Limited ",
+            "Standard Bank Limited",
+            "Standard Chartered Bank",
+            "State Bank of India",
+            "The City Bank Limited",
+            "The Farmers Bank Limited",
+            "The Premier Bank Limited",
+            "Trust Bank Limited",
+            "Union Bank Limited",
+            "United Commercial Bank Limited",
+            "Uttara Bank Limited ",
+            "Woori Bank"});
+            this.bankSelectionComboBox.Location = new System.Drawing.Point(221, 103);
+            this.bankSelectionComboBox.Margin = new System.Windows.Forms.Padding(6);
+            this.bankSelectionComboBox.Name = "bankSelectionComboBox";
+            this.bankSelectionComboBox.Size = new System.Drawing.Size(301, 21);
+            this.bankSelectionComboBox.TabIndex = 16;
+            // 
+            // LimitCreationReportUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(598, 287);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.accountNoComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bankSelectionComboBox);
+            this.Name = "LimitCreationReportUI";
+            this.Text = "LimitCreationReportUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox accountNoComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox bankSelectionComboBox;
+    }
+}
