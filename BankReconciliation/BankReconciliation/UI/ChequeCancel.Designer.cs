c2420c7d099e1dd5b59f3196406b17f63aff703b
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-11-28 14:03:51 +0600

    UI modified

--- BankReconciliation/BankReconciliation/UI/ChequeCancel.Designer.cs
+++ BankReconciliation/BankReconciliation/UI/ChequeCancel.Designer.cs
@@ -43,7 +43,7 @@
             // bankNameComboBox
             // 
             this.bankNameComboBox.FormattingEnabled = true;
-            this.bankNameComboBox.Location = new System.Drawing.Point(340, 64);
+            this.bankNameComboBox.Location = new System.Drawing.Point(232, 60);
             this.bankNameComboBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.bankNameComboBox.Name = "bankNameComboBox";
             this.bankNameComboBox.Size = new System.Drawing.Size(406, 29);
@@ -54,7 +54,7 @@
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Crimson;
-            this.button1.Location = new System.Drawing.Point(290, 268);
+            this.button1.Location = new System.Drawing.Point(353, 254);
             this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 38);
@@ -66,7 +66,7 @@
             // cmbAccountDNo
             // 
             this.cmbAccountDNo.FormattingEnabled = true;
-            this.cmbAccountDNo.Location = new System.Drawing.Point(340, 108);
+            this.cmbAccountDNo.Location = new System.Drawing.Point(232, 104);
             this.cmbAccountDNo.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.cmbAccountDNo.Name = "cmbAccountDNo";
             this.cmbAccountDNo.Size = new System.Drawing.Size(406, 29);
@@ -76,7 +76,7 @@
             // chequeComboBox
             // 
             this.chequeComboBox.FormattingEnabled = true;
-            this.chequeComboBox.Location = new System.Drawing.Point(340, 153);
+            this.chequeComboBox.Location = new System.Drawing.Point(232, 149);
             this.chequeComboBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.chequeComboBox.Name = "chequeComboBox";
             this.chequeComboBox.Size = new System.Drawing.Size(406, 29);
@@ -87,7 +87,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FloralWhite;
-            this.label1.Location = new System.Drawing.Point(147, 64);
+            this.label1.Location = new System.Drawing.Point(39, 60);
             this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(107, 22);
@@ -99,7 +99,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FloralWhite;
-            this.label2.Location = new System.Drawing.Point(145, 108);
+            this.label2.Location = new System.Drawing.Point(37, 104);
             this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(106, 22);
@@ -111,7 +111,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FloralWhite;
-            this.label3.Location = new System.Drawing.Point(157, 153);
+            this.label3.Location = new System.Drawing.Point(49, 149);
             this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(106, 22);
@@ -120,7 +120,7 @@
             // 
             // causeTextBox
             // 
-            this.causeTextBox.Location = new System.Drawing.Point(340, 198);
+            this.causeTextBox.Location = new System.Drawing.Point(232, 194);
             this.causeTextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.causeTextBox.MaxLength = 50;
             this.causeTextBox.Name = "causeTextBox";
@@ -132,7 +132,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FloralWhite;
-            this.label4.Location = new System.Drawing.Point(157, 198);
+            this.label4.Location = new System.Drawing.Point(49, 194);
             this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(61, 22);
@@ -144,7 +144,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Crimson;
-            this.ClientSize = new System.Drawing.Size(987, 477);
+            this.ClientSize = new System.Drawing.Size(759, 390);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.causeTextBox);
             this.Controls.Add(this.label3);
