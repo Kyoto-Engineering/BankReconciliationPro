62530cde928494340dc877bc5e138ac086ee1d4b
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-11-28 11:51:25 +0600

    UI added

--- BankReconciliation/BankReconciliation/UI/Step1Report1.Designer.cs
+++ BankReconciliation/BankReconciliation/UI/Step1Report1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BankReconciliation.UI
 {
-    partial class Step1Report1
+    partial class AccountWiseTransaction
     {
         /// <summary>
         /// Required designer variable.
@@ -28,37 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AccountWiseTransaction));
             this.label2 = new System.Windows.Forms.Label();
+            this.accountNoComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bankSelectionComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(199, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Step1 Reconsiliation ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 87);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 158);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Step1 Reconsiliation ";
+            this.label2.Size = new System.Drawing.Size(120, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Account No";
             // 
-            // Step1Report1
+            // accountNoComboBox
+            // 
+            this.accountNoComboBox.FormattingEnabled = true;
+            this.accountNoComboBox.Location = new System.Drawing.Point(178, 163);
+            this.accountNoComboBox.Margin = new System.Windows.Forms.Padding(6);
+            this.accountNoComboBox.Name = "accountNoComboBox";
+            this.accountNoComboBox.Size = new System.Drawing.Size(301, 21);
+            this.accountNoComboBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 102);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Bank Name";
+            // 
+            // bankSelectionComboBox
+            // 
+            this.bankSelectionComboBox.FormattingEnabled = true;
+            this.bankSelectionComboBox.Items.AddRange(new object[] {
+            "AB Bank Limited",
+            "Agrani Bank Limited",
+            "Al-Arafah Islami Bank Limited ",
+            "Bangladesh Commerce Bank Limited",
+            "Bangladesh Development Bank Limited",
+            "Bangladesh Krishi Bank Limited",
+            "Bank Al-Falah",
+            "Bank Asia Limited",
+            "BASIC Bank Limited",
+            "BRAC Bank Limited",
+            "Citibank NA",
+            "Commercial Bank of Ceylon",
+            "Dhaka Bank Limited",
+            "Dutch Bangla Bank Limited",
+            "Eastern Bank Limited",
+            "Export Import Bank of Bangladesh Limited",
+            "First Security Islami Bank Limited",
+            "Habib Bank Limited",
+            "HSBC (The Hong Kong and Shanghai Banking Corporation Ltd.)",
+            "ICB Islamic Bank Limited",
+            "IFIC Bank Limited",
+            "Islami Bank Bangladesh Limited",
+            "Jamuna Bank Limited",
+            "Janata Bank Limited",
+            "Meghna Bank Limited",
+            "Mercantile Bank Limited",
+            "Midland Bank Limited",
+            "Modhumoti Bank Limited",
+            "Mutual Trust Bank Limited",
+            "National Bank Limited",
+            "National Bank of Pakistan",
+            "NCC Bank Limited",
+            "NRB Bank Limited",
+            "NRB Commercial Bank Limited",
+            "NRB Global Bank Limited",
+            "One Bank Limited",
+            "Palli Sanchay Bank",
+            "Prime Bank Limited",
+            "Pubali Bank Limited",
+            "Rajshahi Krishi Unnoyon Bank (RKUB)",
+            "Rupali Bank Limited",
+            "Shahjalal islami Bank Limited",
+            "Shimanto Bank Limited",
+            "Social Islami Bank Limited ",
+            "Sonali Bank Limited ",
+            "South Bangla Agriculture and Commerce Bank Limited ",
+            "Southeast Bank Limited ",
+            "Standard Bank Limited",
+            "Standard Chartered Bank",
+            "State Bank of India",
+            "The City Bank Limited",
+            "The Farmers Bank Limited",
+            "The Premier Bank Limited",
+            "Trust Bank Limited",
+            "Union Bank Limited",
+            "United Commercial Bank Limited",
+            "Uttara Bank Limited ",
+            "Woori Bank"});
+            this.bankSelectionComboBox.Location = new System.Drawing.Point(178, 107);
+            this.bankSelectionComboBox.Margin = new System.Windows.Forms.Padding(6);
+            this.bankSelectionComboBox.Name = "bankSelectionComboBox";
+            this.bankSelectionComboBox.Size = new System.Drawing.Size(301, 21);
+            this.bankSelectionComboBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.5F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(97, 33);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(279, 26);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Select Bank and Account";
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Snow;
+            this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.5F, System.Drawing.FontStyle.Bold);
+            this.button10.ForeColor = System.Drawing.Color.Green;
+            this.button10.Location = new System.Drawing.Point(324, 217);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(138, 51);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Get";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // AccountWiseTransaction
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(387, 355);
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(498, 300);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.accountNoComboBox);
             this.Controls.Add(this.label1);
-            this.Name = "Step1Report1";
-            this.Text = "Step1Report1";
+            this.Controls.Add(this.bankSelectionComboBox);
+            this.Name = "AccountWiseTransaction";
+            this.Text = "AccountWiseTransaction";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,7 +182,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox accountNoComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox bankSelectionComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button10;
+
     }
 }
