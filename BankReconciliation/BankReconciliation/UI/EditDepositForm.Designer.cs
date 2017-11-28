4a8427cf362f1453340d1aa932f619f6f643abb0
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-11-28 14:05:00 +0600

    UI modified

--- BankReconciliation/BankReconciliation/UI/EditDepositForm.Designer.cs
+++ BankReconciliation/BankReconciliation/UI/EditDepositForm.Designer.cs
@@ -184,7 +184,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(165, 363);
+            this.button1.Location = new System.Drawing.Point(131, 359);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 16;
@@ -196,7 +196,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(870, 440);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(510, 440);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.showCurrentBalanceTextBox);
             this.Controls.Add(this.showCreditBalanceTextBox);
