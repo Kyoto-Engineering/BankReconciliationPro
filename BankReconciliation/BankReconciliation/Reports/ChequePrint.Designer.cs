f40b803d654545e9d20a9ad43a697a1cccf41b96
Fariha Hossain <farihafiha@gmail.com>
2017-11-28 15:21:23 +0600

    tab order

--- BankReconciliation/BankReconciliation/Reports/ChequePrint.Designer.cs
+++ BankReconciliation/BankReconciliation/Reports/ChequePrint.Designer.cs
@@ -56,7 +56,7 @@
             this.chequeComboBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.chequeComboBox.Name = "chequeComboBox";
             this.chequeComboBox.Size = new System.Drawing.Size(342, 29);
-            this.chequeComboBox.TabIndex = 1;
+            this.chequeComboBox.TabIndex = 2;
             // 
             // label1
             // 
@@ -87,7 +87,7 @@
             this.cmbAccountDNo.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.cmbAccountDNo.Name = "cmbAccountDNo";
             this.cmbAccountDNo.Size = new System.Drawing.Size(342, 29);
-            this.cmbAccountDNo.TabIndex = 4;
+            this.cmbAccountDNo.TabIndex = 1;
             this.cmbAccountDNo.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label3
@@ -108,7 +108,7 @@
             this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(167, 66);
-            this.button1.TabIndex = 6;
+            this.button1.TabIndex = 3;
             this.button1.Text = "Print";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -120,7 +120,7 @@
             this.button2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(190, 66);
-            this.button2.TabIndex = 7;
+            this.button2.TabIndex = 4;
             this.button2.Text = "Done";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
