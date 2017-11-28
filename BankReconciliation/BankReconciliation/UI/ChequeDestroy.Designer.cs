386f90e94c2226381dbcc4c39ffbafeefadabe4c
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-11-28 14:04:01 +0600

    UI modified

--- BankReconciliation/BankReconciliation/UI/ChequeDestroy.Designer.cs
+++ BankReconciliation/BankReconciliation/UI/ChequeDestroy.Designer.cs
@@ -126,7 +126,7 @@
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(227, 213);
+            this.button1.Location = new System.Drawing.Point(281, 199);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 35);
             this.button1.TabIndex = 8;
