3f5f760b1dc6e294050b07a9ba0377fc9bba2e23
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-11-28 11:49:07 +0600

    UI added

--- BankReconciliation/BankReconciliation/UI/Step1Reconciliation.Designer.cs
+++ BankReconciliation/BankReconciliation/UI/Step1Reconciliation.Designer.cs
@@ -72,9 +72,14 @@
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.label12 = new System.Windows.Forms.Label();
             this.textBox8 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -94,9 +99,9 @@
             this.Column11,
             this.Column12,
             this.Column13});
-            this.dataGridView1.Location = new System.Drawing.Point(26, 122);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 116);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1143, 151);
+            this.dataGridView1.Size = new System.Drawing.Size(1183, 151);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
@@ -379,7 +384,7 @@
             this.BankdateTimePicker.CustomFormat = "dd/MM/yyyy";
             this.BankdateTimePicker.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BankdateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.BankdateTimePicker.Location = new System.Drawing.Point(797, 367);
+            this.BankdateTimePicker.Location = new System.Drawing.Point(718, 352);
             this.BankdateTimePicker.Name = "BankdateTimePicker";
             this.BankdateTimePicker.ShowCheckBox = true;
             this.BankdateTimePicker.Size = new System.Drawing.Size(182, 25);
@@ -391,7 +396,7 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
             this.label7.ForeColor = System.Drawing.Color.Black;
-            this.label7.Location = new System.Drawing.Point(710, 327);
+            this.label7.Location = new System.Drawing.Point(692, 319);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(366, 26);
             this.label7.TabIndex = 17;
@@ -403,9 +408,9 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.Red;
-            this.label8.Location = new System.Drawing.Point(711, 400);
+            this.label8.Location = new System.Drawing.Point(587, 401);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(353, 198);
+            this.label8.Size = new System.Drawing.Size(582, 110);
             this.label8.TabIndex = 19;
             this.label8.Text = resources.GetString("label8.Text");
             // 
@@ -480,15 +485,69 @@
             this.textBox8.TabIndex = 3;
             this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold);
+            this.groupBox2.Location = new System.Drawing.Point(601, 537);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(548, 122);
+            this.groupBox2.TabIndex = 29;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Step 1 Reconciliation Statements";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Snow;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(206, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 49);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Account Wise Transaction";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Snow;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(389, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 49);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Date Wise Transaction";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Snow;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(29, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 49);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "All Transaction";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // button10
             // 
             this.button10.BackColor = System.Drawing.Color.Snow;
             this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
             this.button10.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button10.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button10.Location = new System.Drawing.Point(917, 610);
+            this.button10.ForeColor = System.Drawing.Color.Green;
+            this.button10.Location = new System.Drawing.Point(934, 348);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(129, 59);
+            this.button10.Size = new System.Drawing.Size(100, 33);
             this.button10.TabIndex = 6;
             this.button10.Text = "Update";
             this.button10.UseVisualStyleBackColor = false;
@@ -499,7 +558,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1020, 682);
+            this.ClientSize = new System.Drawing.Size(1199, 682);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.textBox8);
@@ -521,6 +581,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -572,5 +633,9 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
